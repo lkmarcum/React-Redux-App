dbec6fc9d1062dbf5eda92187081c70b903ba3cb
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-15 15:15:58 -0500

    set up store, set up BrewerList, connected list to store

---
+++ redux-app/src/reducers/breweryReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  breweries: [],
+  isLoading: false,
+  error: ""
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
